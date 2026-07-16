3e6abbe1af6c357d8a9cc5abc2b1f5183b24621c
krish11-git <krishofficial1232@gmail.com>
2026-07-16 19:27:07 +0530

    creating user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const VideoSchema = new mongoose.Schema({
+    videoFile: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    thunbnail: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number, // cloudinary url
+        required: true,
+    },
+    views: {
+        type: Number, // cloudinary url
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+},{timestamps:true});
+
+VideoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", VideoSchema);
