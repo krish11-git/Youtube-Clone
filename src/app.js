ff7af75a0c00cc06c33e3f665a69a6d0656b87bd
krish11-git <krishofficial1232@gmail.com>
2026-07-17 19:55:56 +0530

    testing routes are configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended:true,limits:"16kb"}))
 app.use(express.static("public"));
 app.use(cookieParser())
 
+// routes
+
+import userRouter from "./routes/user.route.js";
+
+app.use("/api/v1/user", userRouter);
+
 export { app }
