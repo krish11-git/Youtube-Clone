3000e503dfc67eeb5fb64c16ed30b27f28545053
krish11-git <krishofficial1232@gmail.com>
2026-08-06 20:35:12 +0530

    add user account management routes for changing password, updating account details, and managing profile images

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -18,4 +18,15 @@ router.route("/logout").post(verifyJwt,logoutUser)
 
 router.route("/refresh-token").post(upload.none(),refreshAccessToken)
 
+router.route("/change-password").post(verifyJwt,upload.none(),changeCurrentPassword)
+
+router.route("/update-account").patch(verifyJwt,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/history").get(verifyJwt,getWatchHistory)
+
+
 export default router;
